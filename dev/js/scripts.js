84a0e3d915fed6fe24c11c78230e617029210c5a
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-04-13 14:41:11 -0400

    enter

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,4 +2,4 @@ import { gsap } from "gsap";
 
 gsap.to("#red-circle", { duration: 4, x: 200 });
 
-gsap.from("#blue-square", { duration: 4, scale: 4, alpha: 0, delay: 2, repeat: -1, yoyo: true });
+gsap.from("#blue-square", { duration: 4, x:"+=200" });
